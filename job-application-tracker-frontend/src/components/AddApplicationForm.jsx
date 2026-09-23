470df8de52359b70073f5be88f89375922910374
nguyenhaley <130112171+nguyenhaley@users.noreply.github.com>
2026-09-22 19:02:34 -0700

    added location and application link as additional tracking roles

--- job-application-tracker-frontend/src/components/AddApplicationForm.jsx
+++ job-application-tracker-frontend/src/components/AddApplicationForm.jsx
@@ -3,9 +3,10 @@ import {API_URL} from '../api'
 
 function AddApplicationForm({ onClose, onSuccess }) {
   const [dateApplied, setDateApplied] = useState('')
-  const [source, setSource] = useState('')
   const [companyName, setCompanyName] = useState('')
   const [roleTitle, setRoleTitle] = useState('')
+  const [location, setLocation] = useState('')
+  const [applicationLink, setApplicationLink] = useState('')
 
   const [errorMessage, setErrorMessage] = useState('')
   const [successMessage, setSuccessMessage] = useState('')
@@ -24,9 +25,10 @@ function AddApplicationForm({ onClose, onSuccess }) {
         },
         body: JSON.stringify({
             date_applied: dateApplied,
-            source,
             company_name: companyName,
             role_title: roleTitle,
+            location: location,
+            application_link: applicationLink
         })
     })
 
@@ -49,9 +51,10 @@ function AddApplicationForm({ onClose, onSuccess }) {
         {!successMessage && (
             <>
                 <input type="date" value={dateApplied} onChange={(e) => setDateApplied(e.target.value)} className="border" />
-                <input type="text" value={source} onChange={(e) => setSource(e.target.value)} className="border"/>
                 <input type="text" value={companyName} onChange={(e) => setCompanyName(e.target.value)} className="border"/>
                 <input type="text" value={roleTitle} onChange={(e) => setRoleTitle(e.target.value)} className="border"/>
+                <input type="text" value={location} onChange={(e) => setLocation(e.target.value)} className="border"/>
+                <input type="text" value={applicationLink} onChange={(e) => setApplicationLink(e.target.value)} className="border"/>
                 <br></br>
                 <button type="button" onClick={onClose}>Cancel</button> 
                 <br></br>
